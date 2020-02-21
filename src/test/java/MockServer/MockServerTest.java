2c5e623135774567a0cbb264c9692c2a3b97dc17
KokovDanil <email>
2020-02-21 16:23:07 +0300

    Correct tests

--- src/test/java/MockServer/MockServerTest.java
+++ src/test/java/MockServer/MockServerTest.java
@@ -26,6 +26,7 @@ class MockServerTest{
 
 
 	//Тест на вхождение пользователя в список (правильные данные)
+	//Успех: верные данны пользователя
 	@Test
 	public void test1() throws Exception{
 		User correct = new User("user_1", 1);
@@ -33,58 +34,62 @@ class MockServerTest{
 		User result = this.restTemplate.getForObject("http://localhost:"+ port +"/company/1/users?name=user_1", User.class);
 
 		if (result.getName() == null) System.out.println("Такого пользователя нет, 404");
-		else System.out.println("Пользователь должен быть: " + result.equalsUser(correct) +
+		else System.out.println("Success!\nПользователь должен быть: " + result.equalsUser(correct) +
 								"\nИмя: " + result.getName() +
 								"\nid компании: " + result.getCompanyId());
 	}
 
 	//Тест на вхождение пользователя в список (нет пользователя в данной компании)
+	//Успех: нет пользователя - 404
 	@Test
 	public void test2() throws Exception{
 		User correct = new User("user_1", 1);
 
 		User result = this.restTemplate.getForObject("http://localhost:"+ port +"/company/2/users?name=user_1", User.class);
 
-		if (result.getName() == null) System.out.println("Такого пользователя нет, 404");
+		if (result.getName() == null) System.out.println("Success! Такого пользователя нет, 404");
 		else System.out.println("Пользователь должен быть: " + result.equalsUser(correct) +
 								"\nИмя: " + result.getName() +
 								"\nid компании: " + result.getCompanyId());
 	}
 
 	//тест с пустым именем пользователя
+	//Успех: нет пользователя - 404
 	@Test
 	public void test3() throws Exception{
 		User correct = new User("user_1", 1);
 
 		User result = this.restTemplate.getForObject("http://localhost:"+ port +"/company/2/users?name=", User.class);
 
-		if (result.getName() == null) System.out.println("Такого пользователя нет, 404");
+		if (result.getName() == null) System.out.println("Success! Такого пользователя нет, 404");
 		else System.out.println("Пользователь должен быть: " + result.equalsUser(correct) +
 								"\nИмя: " + result.getName() +
 								"\nid компании: " + result.getCompanyId());
 	}
 
 	//тест, в котором вместо id компании строка
+	//Успех: нет пользователя - 404
 	@Test
 	public void test4() throws Exception{
 		User correct = new User("user_1", 1);
 
 		User result = this.restTemplate.getForObject("http://localhost:"+ port +"/company/asd/users?name=user_1", User.class);
 
-		if (result.getName() == null) System.out.println("Такого пользователя нет, 404");
+		if (result.getName() == null) System.out.println("Success, 404");
 		else System.out.println("Пользователь должен быть: " + result.equalsUser(correct) +
 								"\nИмя: " + result.getName() +
 								"\nid компании: " + result.getCompanyId());
 	}
 
 	//тест, в котором нет id компании
+	//Успех: 404
 	@Test
 	public void test5() throws Exception{
 		User correct = new User("user_1", 1);
 
 		User result = this.restTemplate.getForObject("http://localhost:"+ port +"/company/users?name=user_1", User.class);
 
-		if (result.getName() == null) System.out.println("Такого пользователя нет, 404");
+		if (result.getName() == null) System.out.println("Success, 404");
 		else System.out.println("Пользователь должен быть: " + result.equalsUser(correct) +
 								"\nИмя: " + result.getName() +
 								"\nid компании: " + result.getCompanyId());
--- src/test/java/MockServer/MockServerTestWithVerify.java
+++ src/test/java/MockServer/MockServerTestWithVerify.java
@@ -19,10 +19,13 @@ class MockServerTestWithVerify {
 	Тесты для запросов:
 	1) тест с правильными данными
 	2) тест с данными, где пользователь не из этой компании
+	3) тест с правильным id компании и пустым пользователем
+	4) тест без id компании
 	*/
 
 
 	//тест на вхождение пользователя в список (правильные данные)
+	//Успех: данные пользователя
 	@Test
 	public void test1() throws Exception{
 		int myCompanyId = 1;
@@ -33,14 +36,15 @@ class MockServerTestWithVerify {
 
 		if (result.getCompanyId() == myCompanyId) {
 			if (result.getName() == null) System.out.println("Такого пользователя нет, 404");
-			else System.out.println("Пользователь должен быть: " + result.equalsUser(correct) +
+			else System.out.println("Success!\nПользователь должен быть: " + result.equalsUser(correct) +
 									"\nИмя: " + result.getName() +
 									"\nid компании: " + result.getCompanyId());
 		}
 		else System.out.println("Вы не имеете доступа к этим данным");
 	}
 
-	//тест с данными, где пользователь не из этой компании
+	//тест без id компании
+	//Успех: Нет доступа
 	@Test
 	public void test2() throws Exception{
 		int myCompanyId = 2;
@@ -55,6 +59,44 @@ class MockServerTestWithVerify {
 									"\nИмя: " + result.getName() +
 									"\nid компании: " + result.getCompanyId());
 		}
+		else System.out.println("Success! Вы не имеете доступа к этим данным");
+	}
+
+	//тест с правильным id компании и пустым именем пользователем
+	//Успех: нет такого пользователя
+	@Test
+	public void test3() throws Exception{
+		int myCompanyId = 1;
+
+		User correct = new User("user_1", 1);
+
+		User result = this.restTemplate.getForObject("http://localhost:"+ port +"/company/1/users?name=", User.class);
+
+		if (result.getCompanyId() == myCompanyId) {
+			if (result.getName() == null) System.out.println("Success, 404");
+			else System.out.println("Пользователь должен быть: " + result.equalsUser(correct) +
+									"\nИмя: " + result.getName() +
+									"\nid компании: " + result.getCompanyId());
+		}
 		else System.out.println("Вы не имеете доступа к этим данным");
+	}
+
+	//тест без id компании
+	//Успех: нет доступа, т.к. "не та компания"
+	@Test
+	public void test4() throws Exception{
+		int myCompanyId = 1;
+
+		User correct = new User("user_1", 1);
+
+		User result = this.restTemplate.getForObject("http://localhost:"+ port +"/company/users?name=", User.class);
+
+		if (result.getCompanyId() == myCompanyId) {
+			if (result.getName() == null) System.out.println("404");
+			else System.out.println("Пользователь должен быть: " + result.equalsUser(correct) +
+									"\nИмя: " + result.getName() +
+									"\nid компании: " + result.getCompanyId());
+		}
+		else System.out.println("Success! Вы не имеете доступа к этим данным");
 	}
 }
